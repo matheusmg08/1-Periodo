0c6bedc9b2eeb82f95b93c506117c5f492be8d2d
Matheus <129191137+matheusmg08@users.noreply.github.com>
2024-03-29 09:40:50 -0300

    Delete Estrutura de dados (Alysson)/exercícios em sala directory

--- Estrutura de dados (Alysson)/exercícios em sala/'aula1alisson.py
+++
@@ -1,3 +0,0 @@
-print ('Hello World')
-nome=input('Digite seu nome:')
-print ('Seja bem-vindo {} ' .format(nome))
--- Estrutura de dados (Alysson)/exercícios em sala/aula20.04.py
+++
@@ -1,7 +0,0 @@
-nome = input("Digite o nome: ")
-sexo = input("Digite o sexo F/M: ")
-if sexo == 'M':
-    idade = input("Digite a idade: ")
-
-print(f"{nome} tem {idade} anos.")
-
